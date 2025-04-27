5caf31c1634f7445e71b5030d35d042f38a9a9ec
Ahmad Fernando <ahmadfernando88@gmail.com>
2025-04-27 17:00:56 +0700

    first

---
+++ database/migrations/2025_04_27_094902_create_pesan_masuk_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pesan_masuk', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('email');
+            $table->string('judul');
+            $table->text('pesan');
+            $table->date('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pesan_masuk');
+    }
+};
